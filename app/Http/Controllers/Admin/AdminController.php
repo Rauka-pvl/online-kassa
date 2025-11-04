bd000bee770d802d463f2962cdb649e914ad4b26
Rauka-pvl <mr.redle3@gmail.com>
2025-11-04 15:18:17 +0500

    v0.1.2

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -255,7 +255,7 @@ class AdminController extends Controller
             'name' => 'required|string|max:255',
             'login' => 'required|string|max:255|unique:users,login,' . $user->id,
             'password' => 'nullable|string|min:8|confirmed',
-            'role' => 'required|in:2,3',
+            'role' => 'required|in:2,3,4',
             'specialization' => 'nullable|string|max:255',
             'phone' => 'nullable|string|max:20',
             'is_active' => 'boolean'
