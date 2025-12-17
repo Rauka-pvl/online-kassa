fb80b362fa1ceb8cea937d91bed6f7132bc98363
Rauka-pvl <mr.redle3@gmail.com>
2025-12-17 09:17:23 +0500

    Add favicon to multiple layout views

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,6 +9,9 @@
 
     <title>Админ-панель - {{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Favicon -->
+    <link rel="icon" type="image/jpeg" href="{{ Storage::url('icons/back.jpg') }}">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>A.S.K. MED - Медицинский центр</title>
+    
+    <!-- Favicon -->
+    <link rel="icon" type="image/jpeg" href="{{ Storage::url('icons/back.jpg') }}">
+    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     @vite([
         'resources/sass/app.scss',
