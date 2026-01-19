053565c4e1e27b478fbba7a2bd38abbe1fe8a606
Rauka-pvl <mr.redle3@gmail.com>
2026-01-19 13:24:22 +0500

    test main

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [ClientController::class, 'main'])->name('main');
+Route::get('/main', [ClientController::class, 'main'])->name('main');
 Route::get('/catalog', [ClientController::class, 'catalog'])->name('catalog');
 Route::get('/sub-catalog/{id}', [ClientController::class, 'subCatalog'])->name('sub-catalog');
 Route::get('/services/{id}', [ClientController::class, 'services'])->name('services');
